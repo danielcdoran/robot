c894bfc12fe64a1b269038654fa5fc23db494e91
DanielCDoran <40theridgeway@gmail.com>
2025-09-03 11:34:43 +0100

    initial

--- main.go
+++ main.go
@@ -1,224 +1,75 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type State interface {
-	MoveInDirection(l *StateMachine)
-	TurnRight(l *StateMachine)
-	TurnLeft(l *StateMachine)
-}
-
-type StateRobot int
+type State int
 
 const (
-	Open StateRobot = iota
+	Open State = iota
 	InProgress
 	Closed
 )
 
-type StateData struct {
-	state    StateRobot
-	position RobotDetails
-}
+type Event int
 
-func NewStateData(pos RobotDetails) StateData {
-	object := new(StateData)
-	// object.state = state
-	object.position = pos
-	return *object
-}
-func (p StateData) OutputRobotPosition() string {
-	return p.position.OutputRobotPosition()
-}
-
-type RobotPosition struct {
-	xpos int64
-	ypos int64
-}
-
-func NewRobotPosition(xpos int64, ypos int64) RobotPosition {
-	pos := new(RobotPosition)
-	pos.xpos = xpos
-	pos.ypos = ypos
-	return *pos
-}
-
-type Direction int
-
-const ( // note : directions are clockwise
-	North Direction = iota
-	East
-	South
-	West
+const (
+	StartProgress Event = iota
+	Close
+	Reopen
 )
 
-type RobotDetails struct {
-	position RobotPosition
-	facing   Direction
-}
-
-func NewRobotDetails(position RobotPosition, facing Direction) RobotDetails {
-	pos := new(RobotDetails)
-	pos.position = position
-	pos.facing = facing
-	return *pos
-}
-func (p RobotDetails) String() string {
-	return fmt.Sprintf("%v %v", p.position, p.facing)
-}
-
-func (p RobotDetails) OutputRobotPosition() string {
-	var direction string
-	switch p.facing {
-
-	case 0:
-		direction = "N"
-	case 1:
-		direction = "E"
-	case 2:
-		direction = "S"
-	case 3:
-		direction = "W"
-	default:
-		fmt.Println("Direction %s not in facing list", p.facing)
-	}
-	return fmt.Sprintf("%d %d %s", p.position.xpos, p.position.ypos, direction)
-}
+type Action func()
 
 type StateMachine struct {
-	currentState     State
-	states           map[string]State
-	currentStateData StateData
-	planetScent      [50][50]bool // false - no scent
+	currentState State
+	transitions  map[State]map[Event]State
+	actions      map[State]map[Event]Action
 }
 
-func NewStateMachine(initialState RobotDetails) *StateMachine {
+func NewStateMachine(initialState State) *StateMachine {
 	sm := &StateMachine{
-		currentState: &Action{},
-		states:       make(map[string]State),
+		currentState: initialState,
+		transitions:  make(map[State]map[Event]State),
+		actions:      make(map[State]map[Event]Action),
 	}
-	sm.currentStateData = NewStateData(initialState)
+
+	sm.transitions[Open] = map[Event]State{
+		StartProgress: InProgress,
+	}
+	sm.transitions[InProgress] = map[Event]State{
+		Close: Closed,
+	}
+	sm.transitions[Closed] = map[Event]State{
+		Reopen: Open,
+	}
+
+	sm.actions[Open] = map[Event]Action{
+		StartProgress: func() { fmt.Println("Ticket is now in progress") },
+	}
+	sm.actions[InProgress] = map[Event]Action{
+		Close: func() { fmt.Println("Ticket is now closed") },
+	}
+	sm.actions[Closed] = map[Event]Action{
+		Reopen: func() { fmt.Println("Ticket is reopened") },
+	}
+
 	return sm
 }
-func NewStateMachineNoStartPosition() *StateMachine {
-	sm := &StateMachine{
-		currentState: &Action{},
-		states:       make(map[string]State),
+
+func (sm *StateMachine) SendEvent(event Event) {
+	if newState, ok := sm.transitions[sm.currentState][event]; ok {
+		sm.currentState = newState
+		if action, ok := sm.actions[sm.currentState][event]; ok {
+			action()
+		}
+	} else {
+		fmt.Println("Invalid transition")
 	}
-	// sm.currentStateData = NewStateData(Open, initialState)
-	return sm
-}
-
-func (p StateMachine) String() string {
-	return fmt.Sprintf("%v", p.currentStateData)
-}
-
-func (p StateMachine) RobotInitialPosition(initialPosition RobotDetails) {
-	p.currentStateData.position = initialPosition
-}
-func (p StateMachine) OutputRobotPosition() string {
-	return p.currentStateData.OutputRobotPosition()
-}
-func (p *StateMachine) hasScent() bool {
-	val := p.planetScent[p.currentStateData.position.position.xpos][p.currentStateData.position.position.ypos]
-	return val
-}
-
-func turnLeft(detail RobotDetails) RobotDetails {
-	val := NewRobotDetails(detail.position, detail.facing)
-	newDirection := []Direction{West, North, East, South}
-	val.facing = newDirection[val.facing]
-	return val
-}
-
-func turnRight(detail RobotDetails) RobotDetails {
-	val := NewRobotDetails(detail.position, detail.facing)
-	newDirection := []Direction{East, South, West, North}
-	val.facing = newDirection[val.facing]
-	return val
-}
-
-// If position is scented ie  true then no movement is allowed
-// If new position is outside area then no movement and setto scented
-func moveInDirection(detail RobotDetails, outsideArea *bool) RobotDetails {
-	val := NewRobotDetails(detail.position, detail.facing)
-	xadd := []int64{0, 1, 0, -1}
-	yadd := []int64{1, 0, -1, 0}
-	newxpos := val.position.xpos + xadd[val.facing]
-	newypos := val.position.ypos + yadd[val.facing]
-	if isOutsideArea(newxpos, newypos) {
-		*outsideArea = true
-		return val
-	}
-	*outsideArea = false
-	val.position.xpos = newxpos
-	val.position.ypos = newypos
-	return val
-}
-
-func isOutsideArea(xpos int64, ypos int64) bool {
-	if xpos < 0 {
-		return true
-	}
-	if xpos > 50 {
-
-		return true
-	}
-	if ypos < 0 {
-		return true
-	}
-	if ypos > 50 {
-		return true
-	}
-	return false
-}
-
-// func (sm *StateMachine) setState(s State) {
-// 	sm.currentState = s
-// }
-
-func (sm *StateMachine) TurnLeft() {
-	sm.currentState.TurnLeft(sm)
-}
-func (sm *StateMachine) TurnRight() {
-	sm.currentState.TurnRight(sm)
-}
-func (sm *StateMachine) MoveInDirection() {
-	if sm.hasScent() {
-		return // no movement allowed
-	}
-	sm.currentState.MoveInDirection(sm)
-}
-
-type Action struct{}
-
-func (g Action) MoveInDirection(l *StateMachine) {
-	var outSideArea bool
-	l.currentStateData.position = moveInDirection(l.currentStateData.position, &outSideArea)
-	if outSideArea {
-		l.planetScent[l.currentStateData.position.position.xpos][l.currentStateData.position.position.ypos] = true
-	}
-}
-func (g Action) TurnRight(l *StateMachine) {
-	l.currentStateData.position = turnRight(l.currentStateData.position)
-}
-func (g Action) TurnLeft(l *StateMachine) {
-	l.currentStateData.position = turnLeft(l.currentStateData.position)
 }
 
 func main() {
-	pos := NewRobotPosition(10, 10)
-	det := NewRobotDetails(pos, North)
-	sm := NewStateMachineNoStartPosition()
-	sm.RobotInitialPosition(det)
-	sm.TurnLeft()
-	fmt.Println(sm)
-	sm.TurnRight()
-	fmt.Println(sm)
-	sm.MoveInDirection()
-	fmt.Println(sm)
-	fmt.Println()
-	fmt.Println(sm.OutputRobotPosition())
+	sm := NewStateMachine(Open)
+	sm.SendEvent(StartProgress)
+	sm.SendEvent(Close)
+	sm.SendEvent(Reopen)
 }
