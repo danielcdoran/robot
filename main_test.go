a5fafb1379748f082bd56ae92f53860d550202bf
DanielCDoran <40theridgeway@gmail.com>
2025-09-03 13:31:05 +0100

    Left turn works

---
+++ main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestTurnLeftStartNorth(t *testing.T) {
+	sm := NewStateMachine(North)
+	sm.SendEvent(TurnLeft)
+	result := sm.currentState
+	expected := West
+	if expected != result {
+		t.Errorf("Expected Left turn result %+v different from result  position %+v", expected, result)
+	}
+}
+func TestTurnLeftStartEast(t *testing.T) {
+	sm := NewStateMachine(East)
+	sm.SendEvent(TurnLeft)
+	result := sm.currentState
+	expected := North
+	if expected != result {
+		t.Errorf("Expected Left turn result %+v different from result  position %+v", expected, result)
+	}
+}
+func TestTurnLeftStartSouth(t *testing.T) {
+	sm := NewStateMachine(South)
+	sm.SendEvent(TurnLeft)
+	result := sm.currentState
+	expected := East
+	if expected != result {
+		t.Errorf("Expected Left turn result %+v different from result  position %+v", expected, result)
+	}
+}
+func TestTurnLeftStartWest(t *testing.T) {
+	sm := NewStateMachine(West)
+	sm.SendEvent(TurnLeft)
+	result := sm.currentState
+	expected := South
+	if expected != result {
+		t.Errorf("Expected Left turn result %+v different from result  position %+v", expected, result)
+	}
+}
