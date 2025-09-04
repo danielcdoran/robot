bd92bf4569e775a5ac03fdf48c2eb12e7e87a6f5
DanielCDoran <40theridgeway@gmail.com>
2025-09-04 14:12:27 +0100

    tests changed so  reflects scented and insidearea combinations

--- scented_test.go
+++ scented_test.go
@@ -4,10 +4,36 @@ import (
 	"testing"
 )
 
-func TestMoveXLessThan0(t *testing.T) {
+//  scented = yes outtside are dont movedPos			TestScentedMovedIsOutsideArea
+//  scented = yes  inside area movedPos
+//  scented = no outside area set scented make movedPos   TestOutsideAreaSetScented
+//  scented = no inside area move
+
+func TestOutsideAreaSetScented(t *testing.T) {
+	posTopRight := NewPosition(49, 49)
+	pos := NewPosition(0, 0)
+	sm := NewStateMachine(West, pos) // new so Scented = false
+	sm.SetTopRightCorner(posTopRight)
+	sm.SendEvent(Forward)
+	result := sm.currentState
+	expected := West
+	if expected != result {
+		t.Errorf("Expected direction %+v to be same but got different %+v", expected, result)
+	}
+	expectedPos := NewPosition(-1, 0)
+	if expectedPos != sm.position {
+		t.Errorf("Expected positiond %+v to be same but got different %+v", expectedPos, sm.position)
+	}
+	if !sm.planetScent[0][0] {
+		t.Errorf("Expected scented (0,0) to be true but got %+v", sm.planetScent[0][0])
+	}
+}
+
+func TestScentedMovedIsOutsideArea(t *testing.T) {
 	posTopRight := NewPosition(49, 49)
 	pos := NewPosition(0, 0)
 	sm := NewStateMachine(West, pos)
+	sm.planetScent[0][0] = true
 	sm.SetTopRightCorner(posTopRight)
 	sm.SendEvent(Forward)
 	result := sm.currentState
@@ -24,60 +50,23 @@ func TestMoveXLessThan0(t *testing.T) {
 	}
 }
 
-func TestMoveYLessThan0(t *testing.T) {
+func TestScentedMovedIsInsideArea(t *testing.T) {
 	posTopRight := NewPosition(49, 49)
-	pos := NewPosition(49, 0)
-	sm := NewStateMachine(South, pos)
-	sm.SetTopRightCorner(posTopRight)
-	sm.SendEvent(Forward)
-	result := sm.currentState
-	expected := South
-	if expected != result {
-		t.Errorf("Expected direction %+v to be same but got different %+v", expected, result)
-	}
-	expectedPos := NewPosition(49, 0)
-	if expectedPos != sm.position {
-		t.Errorf("Expected position %+v to be same but got different %+v", expectedPos, sm.position)
-	}
-	if !sm.planetScent[49][0] {
-		t.Errorf("Expected scented (49,0) to be true but got %+v", sm.planetScent[49][0])
-	}
-}
-
-func TestMoveYGTThan50(t *testing.T) {
-	pos := NewPosition(49, 49)
-	sm := NewStateMachine(North, pos)
-	sm.SetTopRightCorner(pos)
-	sm.SendEvent(Forward)
-	result := sm.currentState
-	expected := North
-	if expected != result {
-		t.Errorf("Expected direction %+v to be same but got different %+v", expected, result)
-	}
-	expectedPos := NewPosition(49, 49)
-	if expectedPos != sm.position {
-		t.Errorf("Expected position %+v to be same but got different %+v", expectedPos, sm.position)
-	}
-	if !sm.planetScent[49][49] {
-		t.Errorf("Expected scented (49,0) to be true but got %+v", sm.planetScent[49][49])
-	}
-}
-
-func TestMoveXGTThan50(t *testing.T) {
-	pos := NewPosition(49, 49)
+	pos := NewPosition(0, 0)
 	sm := NewStateMachine(East, pos)
-	sm.SetTopRightCorner(pos)
+	sm.planetScent[0][0] = true
+	sm.SetTopRightCorner(posTopRight)
 	sm.SendEvent(Forward)
 	result := sm.currentState
 	expected := East
 	if expected != result {
 		t.Errorf("Expected direction %+v to be same but got different %+v", expected, result)
 	}
-	expectedPos := NewPosition(49, 49)
+	expectedPos := NewPosition(1, 0)
 	if expectedPos != sm.position {
-		t.Errorf("Expected position %+v to be same but got different %+v", expectedPos, sm.position)
+		t.Errorf("Expected positiond %+v to be same but got different %+v", expectedPos, sm.position)
 	}
-	if !sm.planetScent[49][49] {
-		t.Errorf("Expected scented (49,0) to be true but got %+v", sm.planetScent[49][49])
+	if !sm.planetScent[0][0] {
+		t.Errorf("Expected scented (0,0) to be true but got %+v", sm.planetScent[0][0])
 	}
 }
